3d9babe7ec5d1803db305f525b45a26a89a08d9e
chellacollectibles <chellasales.business@gmail.com>
2026-04-23 22:46:55 -0400

    Create inventory.js

---
+++ inventory.js
@@ -0,0 +1,17 @@
+const inventory = [
+  {
+    title: "Charizard EX",
+    category: "singles",
+    image: "charizard-ex.jpg"
+  },
+  {
+    title: "PSA 10 Pikachu",
+    category: "graded",
+    image: "psa-10-pikachu.jpg"
+  },
+  {
+    title: "Sealed Elite Trainer Box",
+    category: "sealed",
+    image: "sealed-etb.jpg"
+  }
+];
